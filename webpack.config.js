48f499d386fdba7402e521629ab151ce93f6ffeb
Turki Alotaibi <turalotaibi@gmail.com>
2020-10-27 12:37:08 +0300

    islocal removed

--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,41 @@ const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (webpackConfigEnv) => {
-    const orgName = "aec";
-    const defaultConfig = singleSpaDefaults({
-        orgName,
-        projectName: "root-config",
-        webpackConfigEnv,
-    });
+  const orgName = "aec";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+  });
 
-    return webpackMerge.smart(defaultConfig, {
-        // modify the webpack config however you'd like to by adding to this object
-        devServer: {
-            historyApiFallback: true,
+  return webpackMerge.smart(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    devServer: {
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        // ...Right before your file-loader rule
+        // Bypass automatic `.json` file processing for webpack
+        {
+          type: "javascript/auto",
+          test: /\.json$/,
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+          },
         },
-        module: {
-            rules: [
-                // ...Right before your file-loader rule
-                // Bypass automatic `.json` file processing for webpack
-                {
-                    type: "javascript/auto",
-                    test: /\.json$/,
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[ext]",
-                    },
-                },
-            ],
-            // ...
+      ],
+      // ...
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          orgName,
         },
-        plugins: [
-            new HtmlWebpackPlugin({
-                inject: false,
-                template: "src/index.ejs",
-                templateParameters: {
-                    isLocal:
-                        webpackConfigEnv && webpackConfigEnv.isLocal === "true",
-                    orgName,
-                },
-            }),
-        ],
-    });
+      }),
+    ],
+  });
 };
